390c0d90d21f2feef007dc6c15dd599b8b5c86e0
password123456 <sendmefuture@gmail.com>
2016-04-28 10:15:55 +0900

    Create check_next_saturday.py

---
+++ check_next_saturday.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python2.7
+# -*- coding: utf-8 -*-
+__author__ = 'https://github.com/password123456/'
+
+# http://dateutil.readthedocs.io/en/stable/relativedelta.html
+
+import datetime as DT
+import dateutil.relativedelta as REL
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+today = DT.date.today()
+print '오늘은 %s 입니다.' % today
+
+rd = REL.relativedelta(days=1, weekday=REL.SA)
+next_friday = today + rd
+
+print '돌아오는 토요일은 %s 입니다.' % next_friday
